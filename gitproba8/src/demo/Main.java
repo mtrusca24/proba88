a7f9d0a7738def6be1502caf53278cc375df6484
Marius Trusca <MTrusca@luxoft.com>
2018-01-29 17:06:59 +0200

    mmmmmmmm

--- gitproba8/src/demo/Main.java
+++ gitproba8/src/demo/Main.java
@@ -19,6 +19,7 @@ public class Main {
 		System.out.println("alinaaaaaaaaaaaa");
 		
 		System.out.println("alinaaaaaaaaaaaaggggggggggdddddddddddKKKKKKKKKKKKKKKKaaaaaaayyyyyyyyyyyyyyyyeeeeee");
+		System.out.println("alinaaaaaaaaaaaabbbbbbbbbb");
 	}
 
 }
