b8554bae13b5d4ba8032bed4e1ee04e1a1d54965
Marius Trusca <MTrusca@luxoft.com>
2018-01-29 15:41:33 +0200

    initial

---
+++ gitproba8/src/demo/Ex1.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Ex1 {
+	
+	
+	public static void metoda1() {
+		System.out.println("ce faci din clasa Ex1");
+	}
+
+}
---
+++ gitproba8/src/demo/Main.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class Main {
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Ex1.metoda1();
+		metoda2();
+	}
+	
+	public static void  metoda2() {
+		System.out.println("sunt in metoda din clasa main");
+		System.out.println("marius si eu si maria");
+		
+	}
+
+}
